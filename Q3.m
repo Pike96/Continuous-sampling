a776e798cb33aed612412f8c5a8a88a364a57a06
Yucheng Lin <Pike96@users.noreply.github.com>
2017-03-30 00:05:51 -0700

    Add time series plot

--- Q3.m
+++ Q3.m
@@ -1,13 +1,14 @@
 clear all; close all; clc;
 
-alpha = 2;
-beta = 0.75;
+alpha = 0.5;
+beta = 0;
 gam = 1;
 delta = 0;
 X = stblrnd(alpha,beta,1,0,1e8,1);
 lim = 5;
 Y = X(X<lim&X>-lim); % Only take numbers within limit
 
+figure(1)
 yyaxis left
 hist(Y,100000)
 
@@ -23,3 +24,11 @@ pmf = stblpdf(t,alpha,beta,gam,delta,'quick');
 plot(t, pmf, 'r');
 ylabel('Theoretical pmf')
 set(gca, 'YColor', 'r')
+
+figure(2)
+plot(X)
+title(['Time series plot with \alpha = ',...
+    num2str(alpha), ', \beta = ',...
+    num2str(beta), ', \gamma = 1, \delta = 0'])
+xlabel('Sample #')
+ylabel('Sample Value')
